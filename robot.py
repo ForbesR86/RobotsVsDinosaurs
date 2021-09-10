716dfd8c06904c27746912f4612c0563d1ff25fe
ForbesR86 <89602329+ForbesR86@users.noreply.github.com>
2021-09-10 10:32:53 -0400

    weapons update
    added 3 weapons

--- robot.py
+++ robot.py
@@ -4,5 +4,13 @@ class Robot:
         self.name = name
         self.maxHealth = randomHealth
         self.weapon = []
+        self.create_weapon()
     def attack(self, dinosaur):
         dinosaur.health -= self.weapon.attackpower
+    def create_weapon(self,):
+        weapon1 = Weapon("Sword", 80)
+        weapon2 = Weapon("Blaster", 100)
+        weapon3 = Weapon("Rifle", 110)
+        self.weapon.append(weapon1)
+        self.weapon.append(weapon2)
+        self.weapon.append(weapon3)
