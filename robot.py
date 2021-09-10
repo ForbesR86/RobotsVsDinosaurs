dd12a8e97c28f8ae344d512e130a92ba00ba9517
ForbesR86 <89602329+ForbesR86@users.noreply.github.com>
2021-09-10 11:00:46 -0400

    Update robot.py

--- robot.py
+++ robot.py
@@ -3,9 +3,10 @@ class Robot:
     def __init__(self, name, randomHealth, weapon):
         self.name = name
         self.maxHealth = randomHealth
-        self.weapon = weapon
+        self.weapon = Weapon("Sword", 80)
 
     def attack(self, dinosaur):
-        # dinosaur.maxHealth -= self.weapon.attackpower
-        dinosaur.maxHealth -= 50
+        dinosaur.maxHealth -= self.weapon.attackpower
+        #dinosaur.maxHealth -= 50
+        print(self.name, 'hits ', dinosaur.name, ' for ', self.attackPower)
     
